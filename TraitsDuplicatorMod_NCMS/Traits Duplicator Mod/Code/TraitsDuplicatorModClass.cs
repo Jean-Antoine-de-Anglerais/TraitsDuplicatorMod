1646b916b624d6633ee33efbe4d66f0504a5de2b
Jean-Antoine d'Anglerais <zzzzzzzorro@mail.ru>
2024-05-07 14:25:19 +0300

    Changed logic of switching between modes of adding and removing features, added Simplified Chinese and Traditional Chinese localizations

--- TraitsDuplicatorMod_NCMS/Traits Duplicator Mod/Code/TraitsDuplicatorModClass.cs
+++ TraitsDuplicatorMod_NCMS/Traits Duplicator Mod/Code/TraitsDuplicatorModClass.cs
@@ -2,6 +2,8 @@
 using HarmonyLib;
 using ReflectionUtility;
 using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
 
 namespace TraitsDuplicatorMod_NCMS
 {
@@ -25,15 +27,19 @@ namespace TraitsDuplicatorMod_NCMS
 
             Localizer.Localization("en", "trait_editor_remove", "Trait Editor now removes traits from a creature");
             Localizer.Localization("ru", "trait_editor_remove", "Теперь редактор черт удаляет черты у существа");
+            Localizer.Localization("cz", "trait_editor_remove", "已切换为移除特质模式，点击特质可删除");
+            Localizer.Localization("ch", "trait_editor_remove", "已切換為移除特質模式，點擊特質可刪除");
 
             Localizer.Localization("en", "trait_editor_add", "Trait Editor now adds traits to the creature");
             Localizer.Localization("ru", "trait_editor_add", "Теперь редактор черт добавляет черты существу");
+            Localizer.Localization("cz", "trait_editor_add", "已切换为添加特质模式，点击特质可叠加");
+            Localizer.Localization("ch", "trait_editor_add", "已切換為添加特質模式，點擊特質可疊加");
 
             harmony.Patch(AccessTools.Method(typeof(TraitsWindow), "useTraitOnActor"),
             prefix: new HarmonyMethod(AccessTools.Method(typeof(Patches), "useTraitOnActor_Prefix")));
 
-            harmony.Patch(AccessTools.Method(typeof(RaceClick), "click"),
-            postfix: new HarmonyMethod(AccessTools.Method(typeof(Patches), "click_Postfix")));
+            harmony.Patch(AccessTools.Method(typeof(UiCreature), "click"),
+            prefix: new HarmonyMethod(AccessTools.Method(typeof(Patches), "click_Prefix")));
         }
     }
 
@@ -64,18 +70,46 @@ namespace TraitsDuplicatorMod_NCMS
             return false;
         }
 
-        public static void click_Postfix()
+        public static bool click_Prefix(UiCreature __instance, ref bool ___dropped, ref Tweener ___tweener_scale, ref float ___initScale, ref Tweener ___tweener_rotation)
         {
-            StaticStuff.isTraitRemoverOn = !StaticStuff.isTraitRemoverOn;
+            if (Config.selectedUnit != null && __instance.doScale)
+            {
+                StaticStuff.isTraitRemoverOn = !StaticStuff.isTraitRemoverOn;
 
-            if (StaticStuff.isTraitRemoverOn)
-            {
-                WorldTip.showNow("trait_editor_remove", true, "top", 1f);
+                if (StaticStuff.isTraitRemoverOn)
+                {
+                    WorldTip.showNow("trait_editor_remove", true, "top", 1f);
+                }
+                else if (!StaticStuff.isTraitRemoverOn)
+                {
+                    WorldTip.showNow("trait_editor_add", true, "top", 1f);
+                }
+
+                if (___dropped)
+                {
+                    return false;
+                }
+                if (__instance.doPlayPunch)
+                {
+                    MusicBox.playSound("event:/SFX/OTHER/Punch", -1f, -1f, false, false);
+                }
+                if (__instance.doSfx != "none" && !string.IsNullOrEmpty(__instance.doSfx) && __instance.doSfx.Contains("event:"))
+                {
+                    MusicBox.playSound(__instance.doSfx, -1f, -1f, false, false);
+                }
+                if (__instance.doScale)
+                {
+                    if (___tweener_scale != null && ___tweener_scale.active)
+                    {
+                        ___tweener_scale.Kill(false);
+                    }
+                    float num = ___initScale * 1.2f;
+                    __instance.transform.localScale = new Vector3(num, num, num);
+                    ___tweener_scale = __instance.transform.DOScale(new Vector3(___initScale, ___initScale, ___initScale), 0.3f).SetEase(Ease.OutBack);
+                }
+                return false;
             }
-            else if (!StaticStuff.isTraitRemoverOn)
-            {
-                WorldTip.showNow("trait_editor_add", true, "top", 1f);
-            }
+            return true;
         }
     }
 
@@ -108,7 +142,7 @@ namespace TraitsDuplicatorMod_NCMS
 
             templanguage = language;
 
-            if (templanguage != "ru" && templanguage != "en")
+            if (templanguage != "ru" && templanguage != "en" && templanguage != "cz" && templanguage != "ch")
             {
                 templanguage = "en";
             }
